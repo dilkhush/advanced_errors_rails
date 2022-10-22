8219cfded7db79f8ca1322d816b100e0e1f3f57a
Dilkhush soni <dilkhushsoni2010@gmail.com>
2022-10-22 22:02:41 +0530

    active model 7 issue

--- lib/advanced_errors_rails/full_messages.rb
+++ lib/advanced_errors_rails/full_messages.rb
@@ -1,22 +1,14 @@
 module AdvancedErrorsRails
   module FullMessages
-    def full_messages_with_ignore_attribute
-      error = ActiveModel::Errors.new @base
-      self.each do |attribute, message|
-        byebug
-        unless message.match(/^\^/)
-          error.add attribute, message
-        else
-          error.add :base, message[1..message.length] #Adding to base will mean that the attribute will not be displayed
-        end
-      end
-      error.full_messages_without_ignore_attribute
+    def full_message_with_ignore_attribute
+      return self.message[1..self.message.length] if self.message.match(/^\^/)
+      full_message_without_ignore_attribute
     end
 
     def self.included(base)
       base.class_eval do
-        alias_method :full_messages_without_ignore_attribute, :full_messages
-        alias_method :full_messages, :full_messages_with_ignore_attribute
+        alias_method :full_message_without_ignore_attribute, :full_message
+        alias_method :full_message, :full_message_with_ignore_attribute
       end
     end
   end
