1c0952314b5c9620946575d0544abcb6faa42710
Dilkhush soni <dilkhushsoni2010@gmail.com>
2022-10-22 22:03:25 +0530

    active mode 7 support

--- lib/advanced_errors_rails.rb
+++ lib/advanced_errors_rails.rb
@@ -2,4 +2,4 @@ require 'active_model'
 require "advanced_errors_rails/version"
 require 'advanced_errors_rails/full_messages'
 
-ActiveModel::Errors.send :include, AdvancedErrorsRails::FullMessages
+ActiveModel::Error.send :include, AdvancedErrorsRails::FullMessages
